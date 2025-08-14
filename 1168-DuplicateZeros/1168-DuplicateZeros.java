868a5a1567a0c4d7d9ce5ccb3d8d9f38d50eaec7
Yati-Tiwari <tiwariyati221@gmail.com>
2025-08-14 11:20:44 +0530

    Create 1168-DuplicateZeros

---
+++ 1168-DuplicateZeros/1168-DuplicateZeros.java
@@ -0,0 +1,14 @@
+// Last updated: 8/14/2025, 11:20:44 AM
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0 && i+1<arr.length){
+                for(int j=arr.length-1;j>i+1;j--){
+                   arr[j]=arr[j-1];
+                }
+                arr[i+1]=0;
+                    i++;
+            }
+        }
+    }
+}
