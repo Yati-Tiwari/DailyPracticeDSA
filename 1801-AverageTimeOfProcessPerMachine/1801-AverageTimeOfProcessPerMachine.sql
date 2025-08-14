d8c756ba2f99dd74011e34715eb92ee3407dcfbb
Yati-Tiwari <tiwariyati221@gmail.com>
2025-08-14 11:19:37 +0530

    Create 1801-AverageTimeOfProcessPerMachine

---
+++ 1801-AverageTimeOfProcessPerMachine/1801-AverageTimeOfProcessPerMachine.sql
@@ -0,0 +1,34 @@
+-- Last updated: 8/14/2025, 11:19:36 AM
+select a1.machine_id,ROUND(AVG(a2.timestamp-a1.timestamp),3)
+as processing_time
+from activity as a1,activity as a2 where a1.machine_id=a2.machine_id
+and a1.process_id=a2.process_id and a1.activity_type='start' 
+and a2.activity_type='end'
+group by a1.machine_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
