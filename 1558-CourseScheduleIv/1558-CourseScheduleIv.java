b5e14dbc6b216ad5c1e8280ac1194ee0e55e1b66
Yati-Tiwari <tiwariyati221@gmail.com>
2025-08-14 11:20:02 +0530

    Create 1558-CourseScheduleIv

---
+++ 1558-CourseScheduleIv/1558-CourseScheduleIv.java
@@ -0,0 +1,23 @@
+// Last updated: 8/14/2025, 11:20:02 AM
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        boolean[][] isPrerequisite = new boolean[numCourses][numCourses];
+        for (int[] prereq : prerequisites) {
+            isPrerequisite[prereq[0]][prereq[1]] = true;
+        }
+        for (int k = 0; k < numCourses; k++) {
+            for (int i = 0; i < numCourses; i++) {
+                for (int j = 0; j < numCourses; j++) {
+                    if (isPrerequisite[i][k] && isPrerequisite[k][j]) {
+                        isPrerequisite[i][j] = true;
+                    }
+                }
+            }
+        }
+        List<Boolean> result = new ArrayList<>();
+        for (int[] query : queries) {
+            result.add(isPrerequisite[query[0]][query[1]]);
+        }
+        return result;
+    }
+}
