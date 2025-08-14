0d657045670865a6c8487a49aee780184b94f0d6
Yati-Tiwari <tiwariyati221@gmail.com>
2025-08-14 11:18:07 +0530

    Create 2572-AppendCharactersToStringToMakeSubsequence

---
+++ 2572-AppendCharactersToStringToMakeSubsequence/2572-AppendCharactersToStringToMakeSubsequence.java
@@ -0,0 +1,16 @@
+// Last updated: 8/14/2025, 11:18:07 AM
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int i=0;
+        int j=0;
+        int sLength=s.length();
+        int tLength=t.length();
+        while (i < sLength && j < tLength) {
+            if (s.charAt(i)==t.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        return tLength-j;
+    }
+}
