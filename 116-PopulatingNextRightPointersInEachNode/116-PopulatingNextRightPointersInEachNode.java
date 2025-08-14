10db7475ef0d803adb5890e5b5e6bc9062dffbae
Yati-Tiwari <tiwariyati221@gmail.com>
2025-08-14 11:26:06 +0530

    Create 116-PopulatingNextRightPointersInEachNode

---
+++ 116-PopulatingNextRightPointersInEachNode/116-PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,44 @@
+// Last updated: 8/14/2025, 11:26:06 AM
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root==null){
+            return null;
+        }
+        Node Leftmost=root;
+        while (Leftmost.left != null) {
+            Node head = Leftmost;
+            while (head != null) {
+                head.left.next = head.right;
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+                head = head.next;
+            }
+            Leftmost=Leftmost.left;
+        }
+        return root;
+    }
+}
