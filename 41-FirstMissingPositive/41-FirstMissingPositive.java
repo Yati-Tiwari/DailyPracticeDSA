21a7403baeefe5540071fd576735229ecbc8c362
Yati-Tiwari <tiwariyati221@gmail.com>
2025-08-14 11:27:07 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.java
@@ -0,0 +1,19 @@
+// Last updated: 8/14/2025, 11:27:07 AM
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1; 
+            }
+        }
+        return n + 1;
+    }
+}
