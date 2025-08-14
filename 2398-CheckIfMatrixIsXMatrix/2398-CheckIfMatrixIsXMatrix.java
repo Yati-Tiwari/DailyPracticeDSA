163e019cd1dffe2e5797f1f2ec0ccbd26dee05db
Yati-Tiwari <tiwariyati221@gmail.com>
2025-08-14 11:18:40 +0530

    Create 2398-CheckIfMatrixIsXMatrix

---
+++ 2398-CheckIfMatrixIsXMatrix/2398-CheckIfMatrixIsXMatrix.java
@@ -0,0 +1,16 @@
+// Last updated: 8/14/2025, 11:18:40 AM
+ class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    if (grid[i][j] == 0) return false;
+                } else {
+                    if (grid[i][j] != 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+}
