93663c5dc20fc5d86d086cded71d5c6385e35398
Yati-Tiwari <tiwariyati221@gmail.com>
2025-08-14 11:22:39 +0530

    Create 665-NonDecreasingArray

---
+++ 665-NonDecreasingArray/665-NonDecreasingArray.java
@@ -0,0 +1,21 @@
+// Last updated: 8/14/2025, 11:22:39 AM
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+                count++;
+                if(count>1){
+                    return false;
+                }
+                if(i==1 || nums[i]>=nums[i-2]){
+                nums[i-1]=nums[i];
+            }
+            else{
+                nums[i]=nums[i-1];
+            }
+            }
+        }
+        return true;
+    }
+}
