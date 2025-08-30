e201406a7599809c069ee24deaa53597bbb18cf5
Yati-Tiwari <tiwariyati221@gmail.com>
2025-08-30 21:11:26 +0530

    Create 2202-MaximizetheTopmostElementAfterKMoves

---
+++ 2202-MaximizetheTopmostElementAfterKMoves/version/java/2202-MaximizetheTopmostElementAfterKMoves_20250830_211121.java
@@ -0,0 +1,15 @@
+// Last updated: 8/30/2025, 9:11:21 PM
+class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> res= new ArrayList<>();
+        res.add(folder[0]);
+        for (int i = 1; i < folder.length; i++) {
+            String last = res.get(res.size() - 1);
+            if (!folder[i].startsWith(last + "/")) {
+                res.add(folder[i]);
+            }
+        }
+        return res;
+    }
+}
