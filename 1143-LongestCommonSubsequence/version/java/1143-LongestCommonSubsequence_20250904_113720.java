187b05c0ffd3521365c978b05c7f43ab71fbedee
Yati-Tiwari <tiwariyati221@gmail.com>
2025-09-04 11:37:26 +0530

    Create 1143-LongestCommonSubsequence

---
+++ 1143-LongestCommonSubsequence/version/java/1143-LongestCommonSubsequence_20250904_113720.java
@@ -0,0 +1,28 @@
+// Last updated: 9/4/2025, 11:37:20 AM
+class Solution {
+    public int change(int amount, int[] coins) {
+     	int[][] dp=new int[amount+1][coins.length];
+		for(int[] a:dp) {
+			Arrays.fill(a, -1);
+		}
+		return Coin_Change(coins,amount,0,dp);
+	}
+	public static int Coin_Change(int[] coin,int amount,int i,int[][] dp) {
+		if(amount==0) {
+			return 1;
+		}
+		if(i==coin.length) {
+			return 0;
+		}
+		if(dp[amount][i] != -1) {
+			return dp[amount][i];
+		}
+		int inc=0,exc=0;
+		if(amount>=coin[i]) {
+			inc=Coin_Change(coin,amount-coin[i],i,dp);
+		}
+		exc=Coin_Change(coin,amount,i+1,dp);
+		return dp[amount][i]=inc+exc;
+	}
+
+}
