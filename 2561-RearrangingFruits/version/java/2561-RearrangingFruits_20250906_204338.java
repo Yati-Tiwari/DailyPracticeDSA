ba3c021577c09745adc1ed8cb37d5780d8f38e5f
Yati-Tiwari <tiwariyati221@gmail.com>
2025-09-06 20:43:44 +0530

    Create 2561-RearrangingFruits

---
+++ 2561-RearrangingFruits/version/java/2561-RearrangingFruits_20250906_204338.java
@@ -0,0 +1,39 @@
+// Last updated: 9/6/2025, 8:43:38 PM
+import java.util.*;
+
+class Solution {
+    public long minCost(int[] basket1, int[] basket2) {
+        Map<Integer, Integer> count = new HashMap<>();
+         for (int fruit : basket1) {
+            count.put(fruit, count.getOrDefault(fruit, 0) + 1);
+        }
+        for (int fruit : basket2) {
+            count.put(fruit, count.getOrDefault(fruit, 0) - 1);
+        }
+
+        List<Integer> toSwap = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            int difference = entry.getValue();
+            if (difference % 2 != 0) {
+                return -1; 
+            }
+            for (int i = 0; i < Math.abs(difference) / 2; i++) {
+                toSwap.add(entry.getKey());
+            }
+        }
+        Collections.sort(toSwap);
+        int min = Integer.MAX_VALUE;
+        for (int fruit : basket1) {
+            min = Math.min(min, fruit);
+        }
+        for (int fruit : basket2) {
+            min = Math.min(min, fruit);
+        }
+        long cost = 0;
+        int n = toSwap.size();
+        for (int i = 0; i < n / 2; i++) {
+            cost += Math.min(toSwap.get(i), 2 * min);
+        }
+        return cost;
+    }
+}
