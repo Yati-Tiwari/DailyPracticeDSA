a749d79de2a92e4ab59e03131fcdc9320b346444
Yati-Tiwari <tiwariyati221@gmail.com>
2025-09-18 12:23:27 +0530

    Create 312-BurstBalloons

---
+++ 312-BurstBalloons/version/java/312-BurstBalloons_20250918_122317.java
@@ -0,0 +1,37 @@
+// Last updated: 9/18/2025, 12:23:17 PM
+class Solution {
+    public int maxCoins(int[] nums) {
+        // very impo...***
+    int[] arr=new int[nums.length+2];
+        int[][] dp=new int[arr.length][arr.length];
+        for(int[] d:dp){
+            Arrays.fill(d,-1);
+        }
+    arr[0]=1;
+    arr[arr.length-1]=1;
+    for(int i=0;i<nums.length;i++){
+        arr[i+1]=nums[i];
+    }
+        return maxCoin(arr,0,arr.length-1,dp);
+        
+    }
+    public static int maxCoin(int[] arr, int i, int j,int[][] dp) {
+		if(i+1==j) { 
+			return 0;
+		}
+		if(dp[i][j]!=-1){
+			return dp[i][j];
+		}
+		
+		int ans=Integer.MIN_VALUE;
+		for(int k=i+1;k<j;k++) {
+			
+			int fs=maxCoin(arr,i,k,dp); 
+			int ss=maxCoin(arr,k,j,dp); 
+			int self=arr[i]*arr[k]*arr[j];
+			ans=Math.max(ans, self+fs+ss);
+		}
+		return dp[i][j]=ans;
+	}
+
+}
