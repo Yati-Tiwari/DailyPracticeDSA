af665a59e58cca54e7efb6c101ccd3bc66bdc5df
Yati-Tiwari <tiwariyati221@gmail.com>
2025-09-20 20:33:05 +0530

    Create 225-ImplementStackusingQueues

---
+++ 225-ImplementStackusingQueues/version/java/225-ImplementStackusingQueues_20250920_203255.java
@@ -0,0 +1,26 @@
+// Last updated: 9/20/2025, 8:32:55 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode ans=new ListNode(0,head);
+        ListNode dummy=ans;
+        for(int i=0;i<n;i++){
+            head=head.next;
+        }
+        while(head != null){
+            head=head.next;
+            dummy=dummy.next;
+        }
+        dummy.next=dummy.next.next;
+        return ans.next;
+    }
+}
