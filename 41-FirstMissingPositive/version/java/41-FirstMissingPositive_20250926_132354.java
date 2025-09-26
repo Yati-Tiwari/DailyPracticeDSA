b7ea48e77fee9f774d7aca431742cb31665c01c3
Yati-Tiwari <tiwariyati221@gmail.com>
2025-09-26 13:23:55 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/version/java/41-FirstMissingPositive_20250926_132354.java
@@ -0,0 +1,26 @@
+// Last updated: 9/26/2025, 1:23:54 PM
+class Solution {
+    public int countPrimes(int n) {
+        if(n<2){
+            return 0;
+        }
+        return PrimeSieve(n);
+    }
+    public static int PrimeSieve(int n) {
+		int[] ans=new int[n];
+		ans[0]=ans[1]=1;
+		for(int i=2;i*i<=ans.length;i++) {
+			if(ans[i]==0) { // i is prime here and its factor 
+				for(int j=2;i*j<ans.length;j++) {
+					ans[i*j]=1;
+				}
+			}
+		}
+		int c=0;
+		for(int i=0;i<ans.length;i++) {
+			if(ans[i]==0) c++;
+		}
+		return c;
+	}
+
+}
