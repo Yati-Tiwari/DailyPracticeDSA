3855d77a8a7c0f85ffe018a3895ed0bb7f432c58
Yati-Tiwari <tiwariyati221@gmail.com>
2025-10-04 13:48:46 +0530

    Create 178-RankScores

---
+++ 178-RankScores/version/java/178-RankScores_20251004_134842.java
@@ -0,0 +1,15 @@
+// Last updated: 10/4/2025, 1:48:42 PM
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int[] steps=new int[n+1];
+        steps[1]=1;
+        steps[2]=2;
+        for(int i=3;i<=n;i++){
+            steps[i]=steps[i-1]+steps[i-2];
+        }
+        return steps[n];
+    }
+}
