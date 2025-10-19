b2fc9116ec807d157bddd49c214d527f8b80e36c
Yati-Tiwari <tiwariyati221@gmail.com>
2025-10-19 12:47:37 +0530

    Create 3147-TakingMaximumEnergyFromtheMysticDungeon

---
+++ 3147-TakingMaximumEnergyFromtheMysticDungeon/version/java/3147-TakingMaximumEnergyFromtheMysticDungeon_20251019_124735.java
@@ -0,0 +1,19 @@
+// Last updated: 10/19/2025, 12:47:35 PM
+class Solution {
+    public int maxDistinctElements(int[] nums, int k) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int count=0;
+        long occupied = Long.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            Long left=(long)nums[i]-k;
+            Long right=(long)nums[i]+k;
+            if(occupied < right){
+                occupied=Math.max(occupied+1,left);
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
