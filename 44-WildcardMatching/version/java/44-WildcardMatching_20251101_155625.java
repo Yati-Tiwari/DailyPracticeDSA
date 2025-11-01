4cc7b68122cb9a8dd023925e936e57361e83c363
Yati-Tiwari <tiwariyati221@gmail.com>
2025-11-01 15:56:27 +0530

    Create 44-WildcardMatching

---
+++ 44-WildcardMatching/version/java/44-WildcardMatching_20251101_155625.java
@@ -0,0 +1,37 @@
+// Last updated: 11/1/2025, 3:56:25 PM
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int i = 0, j = 0;
+        int starIdx = -1, match = 0;
+
+        while (i < s.length()) {
+            //  Characters match or '?'
+            if (j < p.length() && (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+                i++;
+                j++;
+            }
+            //  Pattern has '*'
+            else if (j < p.length() && p.charAt(j) == '*') {
+                starIdx = j;
+                match = i;
+                j++;
+            }
+            // Mismatch but previous '*' exists → backtrack
+            else if (starIdx != -1) {
+                j = starIdx + 1;
+                match++;
+                i = match;
+            }
+            //  Mismatch and no '*' → fail
+            else {
+                return false;
+            }
+        }
+        //  Skip remaining '*' in pattern
+        while (j < p.length() && p.charAt(j) == '*') {
+            j++;
+        }
+
+        return j == p.length();
+    }
+}
