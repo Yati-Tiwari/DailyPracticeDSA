7533dc6135676e08f46b91db00545518df6daccd
Yati-Tiwari <tiwariyati221@gmail.com>
2025-11-05 20:48:06 +0530

    Create 96-UniqueBinarySearchTrees

---
+++ 96-UniqueBinarySearchTrees/version/java/96-UniqueBinarySearchTrees_20251105_204803.java
@@ -0,0 +1,15 @@
+// Last updated: 11/5/2025, 8:48:03 PM
+class Solution {
+    public int numTrees(int n) {
+        int[] dp=new int[n+1];
+        dp[0]=1; //empty tree
+        dp[1]=1; // single node
+        for(int nodes=2;nodes<=n;nodes++){
+            for(int root=1;root<=nodes;root++){
+                dp[nodes]+=dp[root-1] *dp[nodes-root];
+            }
+        }
+        return dp[n];
+    }
+
+}
