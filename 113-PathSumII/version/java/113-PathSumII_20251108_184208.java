42cf39904d2488593a7a4bb416c7b6ebce86ced5
Yati-Tiwari <tiwariyati221@gmail.com>
2025-11-08 18:42:11 +0530

    Create 113-PathSumII

---
+++ 113-PathSumII/version/java/113-PathSumII_20251108_184208.java
@@ -0,0 +1,41 @@
+// Last updated: 11/8/2025, 6:42:08 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> current_path=new ArrayList<>();
+        PathSum_2(root,targetSum,current_path,res);
+        return res;
+ 
+    }
+    public void PathSum_2(TreeNode node,int targetSum,List<Integer> current_path,List<List<Integer>> res){
+        if(node == null){
+            return;
+        }
+        current_path.add(node.val);
+        if(node.left==null && node.right==null && targetSum==node.val){
+            res.add(new ArrayList<>(current_path));
+        }
+        else{
+            PathSum_2(node.left,targetSum-node.val,current_path,res);
+            PathSum_2(node.right,targetSum-node.val,current_path,res);
+        }
+        current_path.remove(current_path.size()-1);
+    }
+    
+    
+}
