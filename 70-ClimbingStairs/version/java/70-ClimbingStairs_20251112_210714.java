38acbb7e8056862bdd22faf03fa91161c2008749
Yati-Tiwari <tiwariyati221@gmail.com>
2025-11-12 21:07:16 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/version/java/70-ClimbingStairs_20251112_210714.java
@@ -0,0 +1,15 @@
+// Last updated: 11/12/2025, 9:07:14 PM
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int[] steps=new int[n+1];
+        steps[1]=1;
+        steps[2]=2;
+        for(int i=3;i<=n;i++){
+            steps[i]=steps[i-1]+steps[i-2];
+        }
+        return steps[n];
+    }
+}
