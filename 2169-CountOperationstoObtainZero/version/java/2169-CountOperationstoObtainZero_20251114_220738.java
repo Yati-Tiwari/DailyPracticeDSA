d70004b9ba95a2b60e633b5e97809a72dfb75c68
Yati-Tiwari <tiwariyati221@gmail.com>
2025-11-14 22:07:40 +0530

    Create 2169-CountOperationstoObtainZero

---
+++ 2169-CountOperationstoObtainZero/version/java/2169-CountOperationstoObtainZero_20251114_220738.java
@@ -0,0 +1,15 @@
+// Last updated: 11/14/2025, 10:07:38 PM
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int count=0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 > num2) {
+                num1=num1-num2;
+            } else {
+                num2 = num2-num1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
