685d3dd50b48ec476d38b501c7090f90904c031e
Yati-Tiwari <tiwariyati221@gmail.com>
2025-11-21 19:50:02 +0530

    Create 946-ValidateStackSequences

---
+++ 946-ValidateStackSequences/version/java/946-ValidateStackSequences_20251121_195001.java
@@ -0,0 +1,16 @@
+// Last updated: 11/21/2025, 7:50:01 PM
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st=new Stack<>();
+        int j=0;
+        for(int i=0;i<pushed.length;i++){
+           st.push(pushed[i]);
+           while(!st.isEmpty() && j<popped.length && st.peek()==popped[j]){
+              st.pop();
+              j++;
+           }
+        }
+        return j==popped.length;
+
+    }
+}
