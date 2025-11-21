b1270e5e85ed977cb6f6ebda2ca1474e80460c4f
Yati-Tiwari <tiwariyati221@gmail.com>
2025-11-21 11:45:49 +0530

    Create 104-MaximumDepthofBinaryTree

---
+++ 104-MaximumDepthofBinaryTree/version/java/104-MaximumDepthofBinaryTree_20251121_114547.java
@@ -0,0 +1,36 @@
+// Last updated: 11/21/2025, 11:45:47 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> st=new Stack<>();
+        while(root!=null || !st.isEmpty()){
+            while(root != null){
+                st.push(root);
+                root=root.left;
+            }
+            root=st.pop();
+            k--;
+            if(k==0){
+                return root.val;
+            }
+            root=root.right;
+
+        }
+        return -1;
+    }
+        
+}
