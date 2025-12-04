812fc61d2209f9d581705cf25f2df82ddfefc9bf
Yati-Tiwari <tiwariyati221@gmail.com>
2025-12-04 20:28:34 +0530

    Create

---
+++ problem-1764860312881/version/java/problem-1764860312881_20251204_202832.java
@@ -0,0 +1,22 @@
+// Last updated: 12/4/2025, 8:28:32 PM
+1class Solution {
+2    public int countCollisions(String directions) {
+3        int n=directions.length();
+4        int count=0;
+5        int i=0,j=n-1;
+6        while (i < n && directions.charAt(i)=='L'){
+7            i++;
+8        }
+9        while (j >= 0 && directions.charAt(j)=='R'){
+10            j--;
+11        }
+12        for (int k=i; k<=j;k++) {
+13            if (directions.charAt(k) != 'S'){
+14                count++;
+15            }
+16        }
+17
+18        return count;
+19    }
+20}
+21
