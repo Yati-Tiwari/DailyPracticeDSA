81a92d986d503200b931f0a5b0e9184918929d05
Yati-Tiwari <tiwariyati221@gmail.com>
2025-12-20 20:09:09 +0530

    Create 686-RepeatedStringMatch

---
+++ 686-RepeatedStringMatch/version/java/686-RepeatedStringMatch_20251220_200910.java
@@ -0,0 +1,20 @@
+// Last updated: 12/20/2025, 8:09:10 PM
+1class Solution {
+2    public int repeatedStringMatch(String a, String b) {
+3        StringBuilder sb = new StringBuilder(a);
+4        int ans=1;
+5        while (sb.length() < b.length()) {
+6            sb.append(a);
+7            ans++;
+8        }
+9        if (sb.indexOf(b)!=-1){
+10            return ans;
+11        }
+12        sb.append(a);
+13        if (sb.indexOf(b)!=-1){
+14            return ans+1;
+15        }
+16        return -1;
+17    }
+18}
+19
