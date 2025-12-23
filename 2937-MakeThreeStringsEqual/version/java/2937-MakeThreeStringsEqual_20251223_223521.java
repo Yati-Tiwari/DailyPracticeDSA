5389f25e049335881160a92204f13bcf9d13b1b0
Yati-Tiwari <tiwariyati221@gmail.com>
2025-12-23 22:35:19 +0530

    Create 2937-MakeThreeStringsEqual

---
+++ 2937-MakeThreeStringsEqual/version/java/2937-MakeThreeStringsEqual_20251223_223521.java
@@ -0,0 +1,18 @@
+// Last updated: 12/23/2025, 10:35:21 PM
+1class Solution {
+2    public int findMinimumOperations(String s1, String s2, String s3) {
+3        int n1=s1.length();
+4        int n2=s2.length();
+5        int n3=s3.length();
+6        int n=Math.min(n1,Math.min(n2,n3));
+7        int i=0;
+8        while(i<n && s1.charAt(i)==s2.charAt(i) && s2.charAt(i) ==s3.charAt(i)){
+9            i++;
+10        }
+11        if(i==0){
+12            return -1;
+13        }
+14        int rslt=(n1-i)+(n2-i)+(n3-i);
+15        return rslt;
+16    }
+17}
