4887ebd63ef3ec2acc590dbd0ba40a026497d5a8
Yati-Tiwari <tiwariyati221@gmail.com>
2026-02-02 11:55:08 +0530

    Create 1290-ConvertBinaryNumberinaLinkedListtoInteger

---
+++ 1290-ConvertBinaryNumberinaLinkedListtoInteger/version/java/1290-ConvertBinaryNumberinaLinkedListtoInteger_20260202_115507.java
@@ -0,0 +1,22 @@
+// Last updated: 2/2/2026, 11:55:07 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public int getDecimalValue(ListNode head) {
+13        int ans=0;
+14        ListNode curr=head;
+15        while (curr!=null) {
+16            ans=ans*2 +curr.val;  //shift left and add bit
+17            curr=curr.next;
+18        }
+19        return ans;
+20    }
+21}
