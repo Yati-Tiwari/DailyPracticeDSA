f9091daa46004ce7d4f7dab321f7bb7c187db4d0
Yati-Tiwari <tiwariyati221@gmail.com>
2026-02-06 11:26:29 +0530

    Create 2550-CountCollisionsofMonkeysonaPolygon

---
+++ 2550-CountCollisionsofMonkeysonaPolygon/version/java/2550-CountCollisionsofMonkeysonaPolygon_20260206_112628.java
@@ -0,0 +1,22 @@
+// Last updated: 2/6/2026, 11:26:28 AM
+1class Solution {
+2    static int MOD=1000000007;
+3    public int monkeyMove(int n){
+4        long pow=modPow(2,n);
+5        long ans=(pow-2+MOD)%MOD;
+6        return (int)ans;
+7    }
+8    public long modPow(long x,long n){
+9        long rslt=1;
+10        x%=MOD;
+11        while(n>0){
+12            if(n%2==1){
+13                rslt=(rslt*x)%MOD;
+14            }
+15            x=(x*x)%MOD;
+16            n=n/2;
+17        }
+18        return rslt;
+19    }
+20}
+21
