8a2ef25194e66d4d63f19ee00bd4552bf86998a7
Yati-Tiwari <tiwariyati221@gmail.com>
2026-04-07 10:16:32 +0530

    Create 2946-MatrixSimilarityAfterCyclicShifts

---
+++ 2946-MatrixSimilarityAfterCyclicShifts/version/java/2946-MatrixSimilarityAfterCyclicShifts_20260407_101632.java
@@ -0,0 +1,22 @@
+// Last updated: 4/7/2026, 10:16:32 AM
+1class Solution {
+2    public char findKthBit(int n, int k) {
+3        if(n==1){
+4            return '0'; 
+5        }
+6        int length=(int)Math.pow(2,n)-1;
+7        int mid=(length / 2)+1;
+8        if(k==mid) {
+9            return '1';
+10        }else if (k < mid){
+11            return findKthBit(n-1,k);
+12        }else{
+13            char bit=findKthBit(n-1,length-k+1);
+14            if(bit=='0'){
+15                return '1';
+16            }else{
+17                return '0';
+18            }
+19        }
+20    }
+21}
